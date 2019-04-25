cffa74eaf5c59bc270465b2e8a0d8f5e33cfd606
Dmitrii Evstiukhin <devstiukhin@griddynamics.com>
2019-04-25 10:31:20 -0700

    fix order of commands and add output

--- create-everyth.ps1
+++ create-everyth.ps1
@@ -6,7 +6,19 @@ $vmssname = 'fpvmss'
 $dbsrvname = 'mysqlfpwp'
 $dbuser = 'fpwpdbuser'
 $dbpassword = 'fpwpDBpass1'
+if($Args[0] -eq 'ssl')
+{
+    mkdir -p keys
+    #Create vpn keys and root CA for vpn gateway
+    openssl genrsa -des3 -out keys/rootCA.key 4096 
+    openssl req -x509 -new -nodes -key keys/rootCA.key -sha256 -days 1024 -out keys/rootCA.crt
 
+    openssl genrsa -out keys/admin.key 2048
+    openssl req -new -sha256 -key keys/admin.key -subj "/C=US/ST=CA/O=MyOrg, Inc./CN=admin" -out keys/admin.csr
+    openssl x509 -req -in keys/admin.csr -CA keys/rootCA.crt -CAkey keys/rootCA.key -CAcreateserial -out keys/admin.crt -days 500 -sha256
+    #Create user certificate for mac user
+    openssl pkcs12 -in keys/admin.crt -inkey keys/admin.key -certfile keys/rootCA.crt -export -out keys/admin.p12
+}
 New-AzResourceGroup -Name $rgname -Location $location -Force
 
 # Create storage account and files share for vmss. Also create blob store to put initial script to
@@ -20,18 +32,6 @@ New-AzStorageContainer -Name $blobname -Context $scontext -ErrorAction Ignore
 Set-AzStorageBlobContent -Container $blobname -File $scriptname -Context $scontext -Force 
 $fileuri = ('https://' + $saname + '.blob.core.windows.net/' + $blobname + '/' + $scriptname)
 
-if($Args[0] -eq 'ssl')
-{
-    #Create vpn keys and root CA for vpn gateway
-    openssl genrsa -des3 -out keys/rootCA.key 4096 
-    openssl req -x509 -new -nodes -key keys/rootCA.key -sha256 -days 1024 -out keys/rootCA.crt
-
-    openssl genrsa -out keys/admin.key 2048
-    openssl req -new -sha256 -key keys/admin.key -subj "/C=US/ST=CA/O=MyOrg, Inc./CN=admin" -out keys/admin.csr
-    openssl x509 -req -in keys/admin.csr -CA keys/rootCA.crt -CAkey keys/rootCA.key -CAcreateserial -out keys/admin.crt -days 500 -sha256
-    #Create user certificate for mac user
-    openssl pkcs12 -in keys/admin.crt -inkey keys/admin.key -certfile keys/rootCA.crt -export -out keys/admin.p12
-}
 $sshkey = Get-Content -Path $HOME/.ssh/id_rsa.pub
 $rootca = cat ./keys/rootCA.crt | base64
 
